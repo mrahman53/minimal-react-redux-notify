ceb3a76eff944e4cee56c7e99db8045a0860a3df
Charles Fulnecky <charles.fulnecky@hbo.com>
2017-02-13 16:54:29 -0500

    Pass custom styles to react-redux-notify.

--- src/index.js
+++ src/index.js
@@ -11,8 +11,23 @@ import notifyReducer from 'react-redux-notify';
 const ICONS = {
     [NOTIFICATION_TYPE_ERROR]: <i className="material-icons">error</i>,
     [NOTIFICATION_TYPE_INFO]: <i className="material-icons">info</i>,
-    [NOTIFICATION_TYPE_SUCCESS]: <i className="material-icons">thumb_up</i>,
+    [NOTIFICATION_TYPE_SUCCESS]: <i className="material-icons">check_circle</i>,
     [NOTIFICATION_TYPE_WARNING]: <i className="material-icons">warning</i>
+};
+
+const styles = {
+    [NOTIFICATION_TYPE_ERROR]: {
+        'notification--error': 'tools-notification tools-notification--error'
+    },
+    [NOTIFICATION_TYPE_INFO]: {
+        'notification--info': 'tools-notification tools-notification--info'
+    },
+    [NOTIFICATION_TYPE_SUCCESS]: {
+        'notification--success': 'tools-notification tools-notification--success'
+    },
+    [NOTIFICATION_TYPE_WARNING]: {
+        'notification--warning': 'tools-notification tools-notification--warning'
+    }
 };
 
 const NEW_NOTIFICATION = 'NEW_NOTIFICATION';
@@ -20,19 +35,21 @@ const NEW_NOTIFICATION = 'NEW_NOTIFICATION';
 const newNotification = (notifyType, message) => ({
     type: NEW_NOTIFICATION,
     payload: {
-        type: notifyType,
-        message: message
+        customStyles: styles[notifyType],
+        message: message,
+        type: notifyType
     }
 });
 
 const notificationEpic = action$ =>
     action$.ofType(NEW_NOTIFICATION)
         .map(action => createNotification({
-            message: action.payload.message,
-            type: action.payload.type,
-            icon: ICONS[action.payload.type],
             canDismiss: true,
-            duration: action.payload.type === NOTIFICATION_TYPE_ERROR ? 0 : 5000
+            customStyles: action.payload.customStyles,
+            duration: action.payload.type === NOTIFICATION_TYPE_ERROR ? 0 : 5000,
+            icon: ICONS[action.payload.type],
+            message: action.payload.message,
+            type: action.payload.type
         }));
 
 export {
